e193761ec2ba5ea6ffc2a338b26e70e2183d9b6b
Sloplerol <100miracle001@gmail.com>
2024-10-20 15:26:47 +0800

    Site updated: 2024-10-20 15:26:47

--- js/events.js
+++ js/events.js
@@ -74,6 +74,47 @@ Fluid.events = {
     });
   },
 
+  // registerScrollTopArrowEvent: function() {
+  //   var topArrow = jQuery('#scroll-top-button');
+  //   if (topArrow.length === 0) {
+  //     return;
+  //   }
+  //   var board = jQuery('#board');
+  //   if (board.length === 0) {
+  //     return;
+  //   }
+  //   var posDisplay = false;
+  //   var scrollDisplay = false;
+  //   // Position
+  //   var setTopArrowPos = function() {
+  //     var boardRight = board[0].getClientRects()[0].right;
+  //     var bodyWidth = document.body.offsetWidth;
+  //     var right = bodyWidth - boardRight;
+  //     posDisplay = right >= 50;
+  //     topArrow.css({
+  //       'bottom': posDisplay && scrollDisplay ? '20px' : '-60px',
+  //       'right' : right - 64 + 'px'
+  //     });
+  //   };
+  //   setTopArrowPos();
+  //   jQuery(window).resize(setTopArrowPos);
+  //   // Display
+  //   var headerHeight = board.offset().top;
+  //   Fluid.utils.listenScroll(function() {
+  //     var scrollHeight = document.body.scrollTop + document.documentElement.scrollTop;
+  //     scrollDisplay = scrollHeight >= headerHeight;
+  //     topArrow.css({
+  //       'bottom': posDisplay && scrollDisplay ? '20px' : '-60px'
+  //     });
+  //   });
+  //   // Click
+  //   topArrow.on('click', function() {
+  //     jQuery('body,html').animate({
+  //       scrollTop: 0,
+  //       easing   : 'swing'
+  //     });
+  //   });
+  // },
   registerScrollTopArrowEvent: function() {
     var topArrow = jQuery('#scroll-top-button');
     if (topArrow.length === 0) {
@@ -83,6 +124,10 @@ Fluid.events = {
     if (board.length === 0) {
       return;
     }
+    var arrowUpIcon = jQuery('#scroll-top-button i');
+    if(arrowUpIcon.length === 0){
+      return;
+    } 
     var posDisplay = false;
     var scrollDisplay = false;
     // Position
@@ -92,8 +137,13 @@ Fluid.events = {
       var right = bodyWidth - boardRight;
       posDisplay = right >= 50;
       topArrow.css({
-        'bottom': posDisplay && scrollDisplay ? '20px' : '-60px',
-        'right' : right - 64 + 'px'
+        'bottom': scrollDisplay ? '20px' : '-60px',
+        'right' : posDisplay ? right - 64 : 8 + 'px',
+        'min-width' : posDisplay ? 40 : 28 + 'px',
+        'min-height' : posDisplay ? 40 : 28 + 'px'
+      });
+      arrowUpIcon.css({
+        'font-size' : posDisplay ? 32 : 20 + 'px'
       });
     };
     setTopArrowPos();
@@ -104,7 +154,7 @@ Fluid.events = {
       var scrollHeight = document.body.scrollTop + document.documentElement.scrollTop;
       scrollDisplay = scrollHeight >= headerHeight;
       topArrow.css({
-        'bottom': posDisplay && scrollDisplay ? '20px' : '-60px'
+        'bottom': scrollDisplay ? '20px' : '-60px'
       });
     });
     // Click
